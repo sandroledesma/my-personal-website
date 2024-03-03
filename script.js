bd7022995a95acb5b22eb33cf80baf3017255d84
Alessandro <sandroledesma@gmail.com>
2024-03-03 17:44:46 -0500

    add script.js

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ h2.textContent = "This content is added by JavaScript";
 
 document.querySelection("body").appendChild(h2);
 
+document.addEventListener("DOMContentLoaded", function() {    
+
+    var imageFigures = document.querySelectorAll('.gallery figure');
+
+    imageFigures.forEach(function(figure) {
+        figure.addEventListener('click', function() {
+            console.log('Image clicked: ' + figure.querySelector('figcaption').textContent);
+        });
+    });
+
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var colorChangeParagraph = document.getElementById('colorChange');
+    
+    colorChangeParagraph.addEventListener('mouseover', function() {
+        colorChangeParagraph.style.color = 'blue';
+    });
+
+    colorChangeParagraph.addEventListener('mouseout', function() {
+        colorChangeParagraph.style.color = 'black';
+    });
+});
